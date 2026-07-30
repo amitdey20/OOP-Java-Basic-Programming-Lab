75551374501240be67826cf4719a245779700c60
amitdey20 <141947522+amitdey20@users.noreply.github.com>
2026-07-30 11:59:31 +0600

    Add files via upload

---
+++ gc2.java
@@ -0,0 +1,8 @@
+class gc2{
+    public static void main(String[] args){
+        int a = 74;
+        int b = 36;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
---
+++ gc5.java
@@ -0,0 +1,7 @@
+public class gc5 {
+    public static void main(String[] args) {
+        int first_number = 25;
+        int second_number = 5;
+        System.out.println(first_number + " x " + second_number + " = " + (first_number * second_number));
+    }
+}
