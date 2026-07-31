4cde75bf2d6e43c41888d06ce03948e04122d0c5
amitdey20 <141947522+amitdey20@users.noreply.github.com>
2026-07-31 21:15:08 +0600

    Add files via upload

---
+++ gc6.java
@@ -0,0 +1,11 @@
+public class code6 {
+    public static void main(String[] args){
+    int first_number= 125;
+    int second_number=24;
+    System.out.println(first_number + " + " + second_number + " = " + (first_number + second_number));
+    System.out.println(first_number + " - " + second_number + " = " + (first_number - second_number));
+    System.out.println(first_number + " x " + second_number + " = " + (first_number * second_number));
+    System.out.println(first_number + " / " + second_number + " = " + (first_number / second_number));
+    System.out.println(first_number + " % " + second_number + " = " + (first_number % second_number));
+    }
+}
